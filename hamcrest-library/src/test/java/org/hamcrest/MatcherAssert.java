886c5a679283abfbc33af7d7dd9017cead6c2272
joe.walnes <joe.walnes@2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2>
2006-12-20 19:49:32 +0000

    Removed unit-tests project and put tests with respective projects

---
+++ hamcrest-library/src/test/java/org/hamcrest/MatcherAssert.java
@@ -0,0 +1,18 @@
+/*  Copyright (c) 2000-2006 hamcrest.org
+ */
+package org.hamcrest;
+
+
+public class MatcherAssert {
+
+    public static <T> void assertThat(T actual, Matcher<T> matcher) {
+        if (!matcher.matches(actual)) {
+            Description description = new StringDescription();
+            description.appendText("\nExpected: ");
+            matcher.describeTo(description);
+            description.appendText("\n    got : ").appendValue(actual).appendText("\n");
+            throw new java.lang.AssertionError(description.toString());
+        }
+    }
+
+}
