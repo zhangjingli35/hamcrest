da45119eab101f2a6e35a4ac41b9c9c2eb781921
joe.walnes <joe.walnes@2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2>
2006-12-20 19:20:55 +0000

    Committed half of Magne Rasmussen's changes for Maven 2 and APT based sugar generator to an experimental branch

---
+++ hamcrest-generator/src/main/java/org/hamcrest/generator/apt/AptOptions.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) CodeHive.org 2006
+ *
+ * Created on Dec 19, 2006
+ *
+ */
+package org.hamcrest.generator.apt;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.sun.mirror.apt.Messager;
+
+public class AptOptions
+{
+    private final static Pattern OPTION = Pattern.compile("-A([^=]+)(?:=(.*))?");
+    
+    private final Messager messager;
+    private final Map<String, String> options;
+
+    
+    public AptOptions(final Map<String, String> aptOptions)
+    {
+        this(null, aptOptions);
+    }
+
+    public AptOptions(final Messager messager, final Map<String, String> aptOptions)
+    {
+        this.messager = messager;
+        this.options = new HashMap<String, String>();
+        for (final String key : aptOptions.keySet())
+        {
+            final Matcher matcher = OPTION.matcher(key);
+            if (matcher.matches())
+            {
+                final String option = matcher.group(1);
+                String value = aptOptions.get(key);
+                if (value == null)
+                {
+                    value = matcher.group(2);
+                }
+                this.options.put(option, value);
+            }
+        }
+    }
+
+    public boolean hasOption(final String key)
+    {
+        return this.options.containsKey(key);
+    }
+
+    public String getOption(final String key)
+    {
+        return this.options.get(key);
+    }
+
+    public String getOption(final String key, final String def)
+    {
+        return getOption(this.messager, key, def, def);
+    }
+
+    public boolean isEnabled(final String key)
+    {
+        return "true".equalsIgnoreCase(getOption(null, key, "true", null));
+    }
+    
+    private String getOption(final Messager msgr, final String key, final String defNull, final String defNotSet)
+    {
+        String value = getOption(key);
+        if (value == null)
+        {
+            String message;
+            if (hasOption(key))
+            {
+                message = "Option: " + key + " is null, ";
+                value = defNull;
+            }
+            else
+            {
+                message = "Option: " + key + " not set, ";
+                value = defNotSet;
+            }
+            message += "using default value: " + value;
+            if (msgr != null)
+            {
+                msgr.printWarning(message);
+            }
+        }
+        return value;
+    }
+}
