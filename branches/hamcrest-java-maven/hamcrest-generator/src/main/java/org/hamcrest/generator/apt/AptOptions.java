9093b68fffd5f6c8f352e77716194a422c259cde
magne.rasmussen <magne.rasmussen@2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2>
2007-01-03 21:33:07 +0000

    - Changed the way the annotation processor computes the formal parameter types, so that it also works in Eclipse. - Reformatted the code.

--- branches/hamcrest-java-maven/hamcrest-generator/src/main/java/org/hamcrest/generator/apt/AptOptions.java
+++ branches/hamcrest-java-maven/hamcrest-generator/src/main/java/org/hamcrest/generator/apt/AptOptions.java
@@ -1,8 +1,4 @@
-/*
- * Copyright (c) CodeHive.org 2006
- *
- * Created on Dec 19, 2006
- *
+/*  Copyright (c) 2000-2007 hamcrest.org
  */
 package org.hamcrest.generator.apt;
 
@@ -13,81 +9,66 @@ import java.util.regex.Pattern;
 
 import com.sun.mirror.apt.Messager;
 
-public class AptOptions
-{
-    private final static Pattern OPTION = Pattern.compile("-A([^=]+)(?:=(.*))?");
-    
-    private final Messager messager;
-    private final Map<String, String> options;
+public class AptOptions {
+	private final static Pattern OPTION = Pattern.compile("-A([^=]+)(?:=(.*))?");
 
-    
-    public AptOptions(final Map<String, String> aptOptions)
-    {
-        this(null, aptOptions);
-    }
+	private final Messager messager;
+	private final Map<String, String> options;
 
-    public AptOptions(final Messager messager, final Map<String, String> aptOptions)
-    {
-        this.messager = messager;
-        this.options = new HashMap<String, String>();
-        for (final String key : aptOptions.keySet())
-        {
-            final Matcher matcher = OPTION.matcher(key);
-            if (matcher.matches())
-            {
-                final String option = matcher.group(1);
-                String value = aptOptions.get(key);
-                if (value == null)
-                {
-                    value = matcher.group(2);
-                }
-                this.options.put(option, value);
-            }
-        }
-    }
+	public AptOptions(final Map<String, String> aptOptions) {
+		this(null, aptOptions);
+	}
 
-    public boolean hasOption(final String key)
-    {
-        return this.options.containsKey(key);
-    }
+	public AptOptions(final Messager messager,
+			final Map<String, String> aptOptions) {
+		this.messager = messager;
+		this.options = new HashMap<String, String>();
+		for (final String key : aptOptions.keySet()) {
+			final Matcher matcher = OPTION.matcher(key);
+			if (matcher.matches()) {
+				final String option = matcher.group(1);
+				String value = aptOptions.get(key);
+				if (value == null) {
+					value = matcher.group(2);
+				}
+				this.options.put(option, value);
+			}
+		}
+	}
 
-    public String getOption(final String key)
-    {
-        return this.options.get(key);
-    }
+	public boolean hasOption(final String key) {
+		return this.options.containsKey(key);
+	}
 
-    public String getOption(final String key, final String def)
-    {
-        return getOption(this.messager, key, def, def);
-    }
+	public String getOption(final String key) {
+		return this.options.get(key);
+	}
 
-    public boolean isEnabled(final String key)
-    {
-        return "true".equalsIgnoreCase(getOption(null, key, "true", null));
-    }
-    
-    private String getOption(final Messager msgr, final String key, final String defNull, final String defNotSet)
-    {
-        String value = getOption(key);
-        if (value == null)
-        {
-            String message;
-            if (hasOption(key))
-            {
-                message = "Option: " + key + " is null, ";
-                value = defNull;
-            }
-            else
-            {
-                message = "Option: " + key + " not set, ";
-                value = defNotSet;
-            }
-            message += "using default value: " + value;
-            if (msgr != null)
-            {
-                msgr.printWarning(message);
-            }
-        }
-        return value;
-    }
+	public String getOption(final String key, final String def) {
+		return getOption(this.messager, key, def, def);
+	}
+
+	public boolean isEnabled(final String key) {
+		return "true".equalsIgnoreCase(getOption(null, key, "true", null));
+	}
+
+	private String getOption(final Messager msgr, final String key,
+			final String defNull, final String defNotSet) {
+		String value = getOption(key);
+		if (value == null) {
+			String message;
+			if (hasOption(key)) {
+				message = "Option: " + key + " is null, ";
+				value = defNull;
+			} else {
+				message = "Option: " + key + " not set, ";
+				value = defNotSet;
+			}
+			message += "using default value: " + value;
+			if (msgr != null) {
+				msgr.printWarning(message);
+			}
+		}
+		return value;
+	}
 }
--- branches/hamcrest-java-maven/hamcrest-generator/src/main/java/org/hamcrest/generator/apt/HamcrestAnnotationProcessor.java
+++ branches/hamcrest-java-maven/hamcrest-generator/src/main/java/org/hamcrest/generator/apt/HamcrestAnnotationProcessor.java
@@ -1,12 +1,9 @@
-/*
- * Copyright (c) CodeHive.org 2006
- *
- * Created on Dec 18, 2006
- *
+/*  Copyright (c) 2000-2007 hamcrest.org
  */
 package org.hamcrest.generator.apt;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,8 +15,6 @@ import org.hamcrest.generator.SugarGenerator;
 
 import com.sun.mirror.apt.AnnotationProcessor;
 import com.sun.mirror.apt.AnnotationProcessorEnvironment;
-import com.sun.mirror.apt.RoundCompleteEvent;
-import com.sun.mirror.apt.RoundCompleteListener;
 import com.sun.mirror.declaration.AnnotationTypeDeclaration;
 import com.sun.mirror.declaration.Declaration;
 import com.sun.mirror.declaration.MethodDeclaration;
@@ -31,151 +26,157 @@ import com.sun.mirror.type.ReferenceType;
 import com.sun.mirror.type.TypeMirror;
 import com.sun.mirror.util.SimpleDeclarationVisitor;
 
-
 public class HamcrestAnnotationProcessor extends SimpleDeclarationVisitor
-        implements AnnotationProcessor
-{
-    private final static Pattern TYPE_PATTERN = Pattern.compile("[^<]+(?:<(.*)>)?");
-    
-    private final AnnotationProcessorEnvironment env;
-    private final AnnotationTypeDeclaration factoryAnnotation;
-    private final SugarGenerator sugarGenerator;
+		implements AnnotationProcessor {
+	private final static Pattern TYPE_PATTERN = Pattern.compile("[^<]+(?:<(.*)>)?");
 
-    public HamcrestAnnotationProcessor(
-            final AnnotationProcessorEnvironment env,
-            final AnnotationTypeDeclaration annotation)
-    {
-        this.env = env;
-        this.factoryAnnotation = annotation;
-        this.sugarGenerator = new SugarGenerator();
-        
-        final AptOptions options = new AptOptions(env.getMessager(), env.getOptions());
-        final String generatedClass = options.getOption("generatedClass", "org.hamcrest.Matchers");
-        try
-        {
-            this.sugarGenerator.addWriter(new HamcrestFactoryWriter(
-                    getPackageName(generatedClass),
-                    getSimpleName(generatedClass),
-                    this.env.getFiler().createSourceFile(generatedClass)));
-        }
-        catch (final IOException ex)
-        {
-            this.env.getMessager().printError("Unable to create source file for: " + generatedClass);
-        }
-        if (options.isEnabled("verbose"))
-        {
-            this.sugarGenerator.addWriter(new QuickReferenceWriter());
-        }
-    }
+	private final AnnotationProcessorEnvironment env;
+	private final AnnotationTypeDeclaration factoryAnnotation;
+	private final SugarGenerator sugarGenerator;
 
-    public void process()
-    {
-        final Collection<Declaration> declarations = this.env.getDeclarationsAnnotatedWith(this.factoryAnnotation);
-        if (declarations.size() > 0)
-        {
-            for (Declaration declaration : declarations)
-            {
-                declaration.accept(this);
-            }
-            try
-            {
-                try
-                {
-                    this.sugarGenerator.generate();
-                }
-                finally
-                {
-                    this.sugarGenerator.close();
-                }
-            }
-            catch (final IOException ex)
-            {
-                this.env.getMessager().printError("Unable to generate factory class:" + ex.getMessage());
-            }
-        }
-    }
-    
-    @Override
-    public void visitMethodDeclaration(MethodDeclaration decl)
-    {
-        if (isFactoryMethod(decl))
-        {
-            this.sugarGenerator.addFactoryMethod(buildFactoryMethod(decl));
-        }
-    }
+	public HamcrestAnnotationProcessor(
+			final AnnotationProcessorEnvironment env,
+			final AnnotationTypeDeclaration annotation) {
+		this.env = env;
+		this.factoryAnnotation = annotation;
+		this.sugarGenerator = new SugarGenerator();
 
-    @Override
-    public void visitDeclaration(Declaration declaration)
-    {
-        this.env.getMessager().printWarning("Should only be possible to annotate methods");
-    }
-    
-    private String getPackageName(final String className)
-    {
-        int dotIndex = className.lastIndexOf(".");
-        return dotIndex == -1 ? "" : className.substring(0, dotIndex);
-    }
-    
-    private String getSimpleName(final String className)
-    {
-        int dotIndex = className.lastIndexOf(".");
-        return className.substring(dotIndex + 1);
-    }
-    
-    protected boolean isFactoryMethod(MethodDeclaration method)
-    {
-        final Collection<Modifier> modifiers = method.getModifiers();
-        final TypeMirror returnType = method.getReturnType();
-        DeclaredType declaredType = this.env.getTypeUtils().getDeclaredType(this.env.getTypeDeclaration("org.hamcrest.Matcher"));
-        
-        return modifiers.contains(Modifier.PUBLIC)
-                && modifiers.contains(Modifier.STATIC)
-                && this.env.getTypeUtils().isAssignable(declaredType, returnType);
-    }
-    
-    private FactoryMethod buildFactoryMethod(MethodDeclaration method)
-    {
-        FactoryMethod result = new FactoryMethod(
-                method.getDeclaringType().getQualifiedName(),
-                method.getSimpleName());
-        
-        for (final TypeParameterDeclaration typeParameter : method.getFormalTypeParameters())
-        {
-            result.addGenericTypeParameter(typeParameter.toString());
-        }
-        
-        final TypeMirror returnType = method.getReturnType();
-        result.setGenerifiedType(getTypeParameter(returnType.toString()));
+		final AptOptions options = new AptOptions(env.getMessager(), env.getOptions());
+		final String generatedClass = options.getOption("generatedClass", "org.hamcrest.Matchers");
+		try {
+			this.sugarGenerator.addWriter(new HamcrestFactoryWriter(
+					getPackageName(generatedClass),
+					getSimpleName(generatedClass),
+					this.env.getFiler().createSourceFile(generatedClass)));
+		} catch (final IOException ex) {
+			printError("Unable to create source file for: " + generatedClass);
+		}
+		if (options.isEnabled("verbose")) {
+			this.sugarGenerator.addWriter(new QuickReferenceWriter());
+		}
+	}
 
-        int paramNumber = 1;
-        final Collection<ParameterDeclaration> parameters = method.getParameters();
-        for (final ParameterDeclaration parameter : parameters)
-        {
-            String type = parameter.getType().toString();
-            // Special case for var args methods.... String[] -> String...
-            if (method.isVarArgs() && paramNumber == parameters.size())
-            {
-                type = type.replaceFirst("\\[\\]$", "...");
-            }
-            result.addParameter(type, parameter.getSimpleName());
-            paramNumber++;
-        }
-        
-        for (final ReferenceType exception : method.getThrownTypes())
-        {
-            result.addException(exception.toString());
-        }
+	public void process() {
+		final Collection<Declaration> declarations = this.env
+				.getDeclarationsAnnotatedWith(this.factoryAnnotation);
+		if (declarations.size() > 0) {
+			try {
+				for (Declaration declaration : declarations) {
+					declaration.accept(this);
+				}
+				try {
+					this.sugarGenerator.generate();
+				} finally {
+					this.sugarGenerator.close();
+				}
+			} catch (final IOException ex) {
+				printError("Unable to generate factory class", ex);
+			}
+		}
+	}
 
-        return result;
-    }
+	@Override
+	public void visitMethodDeclaration(MethodDeclaration decl) {
+		if (isFactoryMethod(decl)) {
+			this.sugarGenerator.addFactoryMethod(buildFactoryMethod(decl));
+		}
+	}
 
-    private String getTypeParameter(final String type)
-    {
-        final Matcher matcher = TYPE_PATTERN.matcher(type);
-        if (matcher.matches())
-        {
-            return matcher.group(1);
-        }
-        return null;
-    }
+	@Override
+	public void visitDeclaration(Declaration declaration) {
+		printWarning("Should only be possible to annotate methods");
+	}
+
+	private String getPackageName(final String className) {
+		int dotIndex = className.lastIndexOf(".");
+		return dotIndex == -1 ? "" : className.substring(0, dotIndex);
+	}
+
+	private String getSimpleName(final String className) {
+		int dotIndex = className.lastIndexOf(".");
+		return className.substring(dotIndex + 1);
+	}
+
+	protected boolean isFactoryMethod(MethodDeclaration method) {
+		final Collection<Modifier> modifiers = method.getModifiers();
+		final TypeMirror returnType = method.getReturnType();
+		DeclaredType declaredType = this.env.getTypeUtils().getDeclaredType(
+				this.env.getTypeDeclaration("org.hamcrest.Matcher"));
+
+		return modifiers.contains(Modifier.PUBLIC)
+				&& modifiers.contains(Modifier.STATIC)
+				&& this.env.getTypeUtils().isAssignable(declaredType,
+						returnType);
+	}
+
+	private FactoryMethod buildFactoryMethod(MethodDeclaration method) {
+		FactoryMethod result = new FactoryMethod(method.getDeclaringType()
+				.getQualifiedName(), method.getSimpleName());
+
+		for (final TypeParameterDeclaration typeParameter : method
+				.getFormalTypeParameters()) {
+			Collection<String> bounds = new ArrayList<String>();
+			for (ReferenceType bound : typeParameter.getBounds()) {
+				final String boundStr = bound.toString();
+				if (!("java.lang.Object".equals(boundStr))) {
+					bounds.add(boundStr);
+				}
+			}
+			StringBuilder type = new StringBuilder();
+			type.append(typeParameter.getSimpleName());
+			boolean first = true;
+			for (String bound : bounds) {
+				if (first) {
+					type.append(" extends ");
+					first = false;
+				} else {
+					type.append(" & ");
+				}
+				type.append(bound.toString());
+			}
+			result.addGenericTypeParameter(type.toString());
+		}
+
+		final TypeMirror returnType = method.getReturnType();
+		result.setGenerifiedType(getTypeParameter(returnType.toString()));
+
+		int paramNumber = 1;
+		final Collection<ParameterDeclaration> parameters = method
+				.getParameters();
+		for (final ParameterDeclaration parameter : parameters) {
+			String type = parameter.getType().toString();
+			// Special case for var args methods.... String[] -> String...
+			if (method.isVarArgs() && paramNumber == parameters.size()) {
+				type = type.replaceFirst("\\[\\]$", "...");
+			}
+			result.addParameter(type, parameter.getSimpleName());
+			paramNumber++;
+		}
+
+		for (final ReferenceType exception : method.getThrownTypes()) {
+			result.addException(exception.toString());
+		}
+
+		return result;
+	}
+
+	private String getTypeParameter(final String type) {
+		final Matcher matcher = TYPE_PATTERN.matcher(type);
+		if (matcher.matches()) {
+			return matcher.group(1);
+		}
+		return null;
+	}
+	
+	private void printError(final String message) {
+		this.env.getMessager().printError(message);
+	}
+	
+	private void printError(final String message, final Throwable throwable) {
+		printError(message + ": " + throwable.getMessage());
+	}
+	
+	private void printWarning(final String message) {
+		this.env.getMessager().printWarning(message);
+	}
 }
