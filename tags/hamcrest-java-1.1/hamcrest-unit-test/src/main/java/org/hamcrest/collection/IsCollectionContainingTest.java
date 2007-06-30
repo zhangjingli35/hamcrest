69d7de9afed9c92c008c48bb5b1cd0d0ca160679
joe.walnes <joe.walnes@2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2>
2007-06-30 12:55:40 +0000

    Tagged 1.1

---
+++ tags/hamcrest-java-1.1/hamcrest-unit-test/src/main/java/org/hamcrest/collection/IsCollectionContainingTest.java
@@ -0,0 +1,56 @@
+package org.hamcrest.collection;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.collection.IsCollectionContaining.hasItem;
+import static org.hamcrest.collection.IsCollectionContaining.hasItems;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import java.util.ArrayList;
+
+import org.hamcrest.AbstractMatcherTest;
+import org.hamcrest.Matcher;
+
+public class IsCollectionContainingTest extends AbstractMatcherTest {
+
+    protected Matcher<?> createMatcher() {
+        return hasItem(equalTo("irrelevant"));
+    }
+
+    public void testMatchesACollectionThatContainsAnElementMatchingTheGivenMatcher() {
+        assertMatches("should matches list that contains 'a'",
+                hasItem(equalTo("a")), asList("a", "b", "c"));
+    }
+
+    public void testDoesNotMatchCollectionThatDoesntContainAnElementMatchingTheGivenMatcher() {
+        assertDoesNotMatch("should not matches list that doesn't contain 'a'",
+                hasItem(equalTo("a")), asList("b", "c"));
+        assertDoesNotMatch("should not matches empty list",
+                hasItem(equalTo("a")), new ArrayList<String>());
+    }
+
+    public void testDoesNotMatchNull() {
+        assertDoesNotMatch("should not matches null", hasItem(equalTo("a")), null);
+    }
+
+    public void testHasAReadableDescription() {
+        assertDescription("a collection containing \"a\"", hasItem(equalTo("a")));
+    }
+
+    public void testMatchesAllItemsInCollection() {
+        assertMatches("should match list containing all of items",
+                hasItems(equalTo("a"), equalTo("b"), equalTo("c")),
+                asList("a", "b", "c"));
+        assertMatches("should match list containing all of items (without matchers)",
+                hasItems("a", "b", "c"),
+                asList("a", "b", "c"));
+        assertMatches("should match list containing all of items in any order",
+                hasItems(equalTo("a"), equalTo("b"), equalTo("c")),
+                asList("c", "b", "a"));
+        assertMatches("should match list containing all of items plus others",
+                hasItems(equalTo("a"), equalTo("b"), equalTo("c")),
+                asList("e", "c", "b", "a", "d"));
+        assertDoesNotMatch("should not match list unless it contains all items",
+                hasItems(equalTo("a"), equalTo("b"), equalTo("c")),
+                asList("e", "c", "b", "d")); // 'a' missing
+    }
+}
---
+++ tags/hamcrest-java-1.1/hamcrest-unit-test/src/main/java/org/hamcrest/core/AnyOfTest.java
@@ -0,0 +1,42 @@
+package org.hamcrest.core;
+
+import org.hamcrest.AbstractMatcherTest;
+import org.hamcrest.Matcher;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.AnyOf.anyOf;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNot.not;
+
+public class AnyOfTest extends AbstractMatcherTest {
+
+    protected Matcher<?> createMatcher() {
+        return anyOf(equalTo("irrelevant"));
+    }
+
+    public void testEvaluatesToTheTheLogicalDisjunctionOfTwoOtherMatchers() {
+        assertThat("good", anyOf(equalTo("bad"), equalTo("good")));
+        assertThat("good", anyOf(equalTo("good"), equalTo("good")));
+        assertThat("good", anyOf(equalTo("good"), equalTo("bad")));
+
+        assertThat("good", not(anyOf(equalTo("bad"), equalTo("bad"))));
+    }
+
+    public void testEvaluatesToTheTheLogicalDisjunctionOfManyOtherMatchers() {
+        assertThat("good", anyOf(equalTo("bad"), equalTo("good"), equalTo("bad"), equalTo("bad"), equalTo("bad")));
+        assertThat("good", not(anyOf(equalTo("bad"), equalTo("bad"), equalTo("bad"), equalTo("bad"), equalTo("bad"))));
+    }
+
+    public void testSupportsMixedTypes() {
+        assertThat(new SampleBaseClass("good"), anyOf(
+                equalTo(new SampleBaseClass("bad")),
+                equalTo(new SampleBaseClass("good")),
+                equalTo(new SampleSubClass("ugly"))
+        ));
+        assertThat(new SampleSubClass("good"), anyOf(
+                equalTo(new SampleBaseClass("bad")),
+                equalTo(new SampleBaseClass("good")),
+                equalTo(new SampleSubClass("ugly"))
+        ));
+    }
+
+}
