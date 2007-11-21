26fe0deeb68f3eb47635746f541a292a30667512
neildunn <neildunn@2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2>
2007-11-21 13:32:52 +0000

    Tagged 1.1

---
+++ tags/hamcrest-java-1.1/hamcrest-java/hamcrest-unit-test/src/main/java/org/hamcrest/object/HasToStringTest.java
@@ -0,0 +1,40 @@
+package org.hamcrest.object;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.object.HasToString.hasToString;
+
+import org.hamcrest.AbstractMatcherTest;
+import org.hamcrest.Matcher;
+import org.hamcrest.StringDescription;
+
+public class HasToStringTest extends AbstractMatcherTest {
+
+    private static final String TO_STRING_RESULT = "toString result";
+    private static final Object ARG = new Object() {
+        public String toString() {
+            return TO_STRING_RESULT;
+        }
+    };
+
+    protected Matcher<?> createMatcher() {
+        return hasToString(equalTo("irrelevant"));
+    }
+
+    public void testPassesResultOfToStringToNestedMatcher() {
+        assertThat(ARG, hasToString(equalTo(TO_STRING_RESULT)));
+        assertThat(ARG, not(hasToString(equalTo("OTHER STRING"))));
+    }
+
+    public void testHasReadableDescription() {
+        Matcher<String> toStringMatcher = equalTo(TO_STRING_RESULT);
+        Matcher<Matcher<String>> matcher = hasToString(toStringMatcher);
+
+        assertEquals("asString(" + descriptionOf(toStringMatcher) + ")", descriptionOf(matcher));
+    }
+
+    private String descriptionOf(Matcher<?> matcher) {
+    	return StringDescription.asString(matcher);
+    }
+}
