4ffaf8e813f14c1f7c94702fcf38d0910e8e0ee5
dharkness <dharkness@2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2>
2010-07-27 22:24:51 +0000

    Tagged release 0.3.0

---
+++ tags/hamcrest-php-0.3.0/tests/bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting(E_ALL | E_STRICT);
+
+if (defined('E_DEPRECATED'))
+{
+  error_reporting(error_reporting() | E_DEPRECATED);
+}
+
+define('HAMCREST_TEST_BASE', realpath(dirname(__FILE__)));
+define('HAMCREST_BASE', realpath(dirname(dirname(__FILE__))));
+
+require_once 'PHPUnit/Framework.php';
+
+set_include_path(implode(PATH_SEPARATOR, array(
+  HAMCREST_TEST_BASE,
+  HAMCREST_BASE,
+  HAMCREST_BASE . '/hamcrest',
+  get_include_path()
+)));
+
+require_once 'hamcrest.php';
